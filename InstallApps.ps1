3767a7dc0389445f23367d171f6e1d3e2e750646
Thiago Reis <thiagoreis128@gmail.com>
2021-01-31 17:04:05 -0300

    Ajuste no script e incluindo mais apps

--- InstallApps.ps1
+++ InstallApps.ps1
@@ -1,7 +1,6 @@
-$chocoAppList = "7zip,winrar,googlechrome,firefox,discord,telegram,crystaldiskinfo,obs,vlc,git,firacode,nodejs,yarn,vscode,choco,install,microsoft-windows-terminal,adobereader,jre8,malwarebytes,skype,ccleaner,dotnet3.5,zoom,winamp,whatsapp,telegram,openvpn,nvidia-display-driver,office2019proplus,opera,internet-download-manager,lightshot.install"
+$chocoAppList = "7zip,winrar,googlechrome,firefox,discord,github-desktop,sublimetext3.app,telegram,driverbooster,crystaldiskinfo,obs,vlc,git,firacode,nodejs,yarn,vscode,choco,install,microsoft-windows-terminal,adobereader,jre8,malwarebytes,skype,ccleaner,dotnet3.5,zoom,winamp,whatsapp,telegram,openvpn,nvidia-display-driver,office2019proplus,opera,internet-download-manager,lightshot.install"
 
-
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false -or [string]::IsNullOrWhiteSpace($dismAppList) -eq $false) {
+if ([string]::IsNullOrWhiteSpace($chocoAppList) -eq $false) {
     try {
         choco config get cacheLocation
     }
@@ -17,6 +16,6 @@ if ([string]::IsNullOrWhiteSpace($chocoAppList) -eq $false) {
 
     foreach ($app in $appsToInstall) {
         Write-Host "Instalando $app"
-        & choco install $app /y /source windowsf
+        & choco install $app $app /y | Write-Output 
     }
 }
